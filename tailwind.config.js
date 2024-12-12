588bd43367f03d7a4c2e905d6daa7d44870b3bf5
SashaCarton <163831379+SashaCarton@users.noreply.github.com>
2024-12-12 19:23:37 +0100

    Ajout du header

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./**/*.php'], // Mets ici les chemins de tes fichiers HTML ou PHP
+  theme: {
+    extend: {
+      fontFamily: {
+        ogg: ['Ogg', 'serif'],
+      },
+    },
+  },
+  plugins: [],
+};
+
